cda457dd51b5c513a8b8efb7c3e171b07e4b01b9
ppx <3375552334@qq.com>
2025-02-12 17:06:46 +0800

    Initial Commit

---
+++ PlayLocalVideo3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayLocalVideo3
+//
+//  Created by ppx on 2025/2/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
